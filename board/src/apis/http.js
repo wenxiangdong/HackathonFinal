33db9899880ae49d760eacb89e73c45205609862
eric <wenxiangdong@outlook.com>
2019-05-26 15:27:44 +0800

    缩进

--- board/src/apis/http.js
+++ board/src/apis/http.js
@@ -4,13 +4,13 @@ import Logger from "../utils/logger";
 
 Axios.defaults.withCredentials = true;
 Axios.defaults.headers = {
-    'Content-Type': "application/json;charset=utf8"
+  'Content-Type': "application/json;charset=utf8"
 };
 
 interface HttpResponse<T> {
-    data: T;
-    code: String;
-    message: String;
+  data: T;
+  code: String;
+  message: String;
 }
 
 /**
@@ -18,53 +18,55 @@ interface HttpResponse<T> {
  * @type {{SUCCESS: string, ERROR: string}}
  */
 export const HttpCodes = {
-    SUCCESS: "SUCCESS",
-    ERROR: "ERROR"
+  SUCCESS: "SUCCESS",
+  ERROR: "ERROR"
 };
 
 export class Http {
-    static _baseUrl = "http://localhost/nju_admit_server";
-    static _logger = Logger.getLogger("http");
-    static async _handleResult<T>(responsePromise: AxiosPromise<AxiosResponse<T>>): Promise<T> {
-        const response = await responsePromise;
-        this._logger.info("请求得到", response);
-        if (response.status !== 200) {
-            throw new Error("网络错误");
-        } else {
-            const httpResponse: HttpResponse =  response.data;
-            if (httpResponse.code !== HttpCodes.SUCCESS) {
-                throw httpResponse;
-            } else {
-                return httpResponse.data;
-            }
-        }
-    }
+  static _baseUrl = "http://localhost/nju_admit_server";
+  static _logger = Logger.getLogger("http");
 
-    static async post<T>(url: String, params: Object = {}): Promise<T> {
-        this._logger.info(`请求${url}，参数：${JSON.stringify(params)}`);
-        url = this._baseUrl + url;
-        return this._handleResult(Axios.post(url, params));
+  static async _handleResult<T>(responsePromise: AxiosPromise<AxiosResponse<T>>): Promise<T> {
+    const response = await responsePromise;
+    this._logger.info("请求得到", response);
+    if (response.status !== 200) {
+      throw new Error("网络错误");
+    } else {
+      const httpResponse: HttpResponse = response.data;
+      if (httpResponse.code !== HttpCodes.SUCCESS) {
+        throw httpResponse;
+      } else {
+        return httpResponse.data;
+      }
     }
-    static async get<T>(url: String, params: Object = {}): Promise<T> {
-        this._logger.info(`请求${url}，参数：${JSON.stringify(params)}`);
-        url = this._baseUrl + url;
-        return this._handleResult(
-            Axios.get(url, {
-                params: params
-            })
-        );
-    }
+  }
+
+  static async post<T>(url: String, params: Object = {}): Promise<T> {
+    this._logger.info(`请求${url}，参数：${JSON.stringify(params)}`);
+    url = this._baseUrl + url;
+    return this._handleResult(Axios.post(url, params));
+  }
+
+  static async get<T>(url: String, params: Object = {}): Promise<T> {
+    this._logger.info(`请求${url}，参数：${JSON.stringify(params)}`);
+    url = this._baseUrl + url;
+    return this._handleResult(
+      Axios.get(url, {
+        params: params
+      })
+    );
+  }
 }
 
 /**
  * 模拟请求
  */
 export class HttpMock {
-    static success<T>(data: T): Promise<T> {
-        return new Promise<T>(resolve => {
-            setTimeout(() => {
-                resolve(data);
-            }, 1000);
-        })
-    }
+  static success<T>(data: T): Promise<T> {
+    return new Promise<T>(resolve => {
+      setTimeout(() => {
+        resolve(data);
+      }, 1000);
+    })
+  }
 }
